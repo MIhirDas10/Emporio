10667a7e2afd7051294cfb7f0f1e7cfcb3b8e656
MIhirDas10 <dasmihir2911@gmail.com>
2025-09-12 17:32:18 +0600

    axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: import.meta.env.PROD
+    ? "https://projectemporio.vercel.app/api"
+    : "http://localhost:5000/api",
   withCredentials: true,
 });
 
